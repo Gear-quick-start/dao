e230352797ffd6d04d7519d46b0fe7479d89a0e0
Louisa <luiza.medova@phystech.edu>
2022-10-24 19:46:45 +0300

    unused function

--- src/ft_messages.rs
+++ src/ft_messages.rs
@@ -33,15 +33,3 @@ pub async fn transfer_tokens(
         }
     }
 }
-
-pub async fn balance(token_id: &ActorId, account: &ActorId) -> u128 {
-    let reply =
-        msg::send_for_reply_as::<_, FTokenEvent>(*token_id, FTokenAction::GetBalance(*account), 0)
-            .expect("Error in sending a message `FTokenAction::GetBalance`")
-            .await
-            .expect("Unable to decode `FTokenEvent`");
-    match reply {
-        FTokenEvent::Balance(balance_response) => balance_response,
-        _ => 0,
-    }
-}
